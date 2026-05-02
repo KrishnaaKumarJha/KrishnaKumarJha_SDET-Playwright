8a4ea618f07c06c54006bf2c7e6db9cbdb511d51
KrishnaaKumarJha <krishnakrsindia@gmail.com>
2026-05-02 11:22:40 +0530

    Random,Type casting,Array Operations

--- Day2/index.js
+++ Day2/index.js
@@ -25,3 +25,41 @@ console.log(arr.find(x => x === "Krishna"));
 
 //filter and print each element multiplied by 2
 arr.filter(x => typeof x === "number").map(x => x * 2).forEach(x => console.log(`Elements after multiplied by * 2 ${x}`));
+
+// Random number between 10 and 60
+console.log(Math.floor(Math.random() * 51) + 10);
+
+// Floor and Ceil
+let decimalVal = 3.7;
+console.log(Math.floor(decimalVal));
+console.log(Math.ceil(decimalVal));
+
+// Type conversions
+let strValue = "8";
+console.log(Number(strValue));
+console.log(String(12));
+
+let deciVal = "3.7";
+console.log(parseInt(deciVal));
+console.log(parseFloat(deciVal));
+
+// Array operations
+let fruits = ["apple", "banana", "mango"];
+
+console.log(fruits.indexOf("orange"));
+console.log(fruits.includes("orange"));
+console.log(fruits.length);
+console.log(fruits[3]);
+console.log(fruits[fruits.length - 1]);
+
+fruits.push("orange");
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+fruits.unshift("grapes");
+console.log(fruits);
+
+fruits.shift();
+console.log(fruits);

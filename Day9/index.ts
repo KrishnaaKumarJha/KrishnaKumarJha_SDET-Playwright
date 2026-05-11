1f27345b8408d45460ae04726ae55b8fd170849c
KrishnaaKumarJha <krishnakrsindia@gmail.com>
2026-05-11 12:48:53 +0530

    Variable_Types_TypeScript

--- Day9/index.ts
+++ Day9/index.ts
@@ -1,2 +1,17 @@
 let x: number = 10;
 console.log(x,typeof(x));
+
+//Multiple Ways to define a variable
+let a: number | string;
+
+a = 12;
+console.log(`Valuse of a as a number: ${a}`);
+
+a = "Krishna";
+console.log(`Valuse of a as a String: ${a}`);
+
+//Creating a function with defined variable type
+function add (a: number, b: number): number{
+    return a+b;
+};
+console.log(add(7,7));

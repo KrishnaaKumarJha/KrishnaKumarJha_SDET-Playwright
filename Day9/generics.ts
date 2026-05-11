fe55c27257e390443ae531a606e4f639ade9fd9e
KrishnaaKumarJha <krishnakrsindia@gmail.com>
2026-05-11 15:44:45 +0530

    generics_With_Constraints

--- Day9/generics.ts
+++ Day9/generics.ts
@@ -77,3 +77,17 @@ names.addItems("Aditya");
 names.addItems("Ansh");
 
 console.log(names.getItem(0), names.getItem(1), names.getItem(2));
+
+//generic with constraints
+interface HasLength {
+    length: number;
+}
+
+// generics with contraints | extends keyword
+function logLength<T extends HasLength>(arg: T): void { // T -> property .length --> string, arrays
+    console.log(arg.length);
+}
+
+logLength("Krishna"); // string --> length
+logLength([1, 2, 3]); // array --> length
+// logLength(10); // number --> X
